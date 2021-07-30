5f634fefca8acc647351facc9aacc8a0bad27996
Jasper Goce <jaspergoce@comfortdelgro.com>
2021-07-30 21:07:19 +0800

    Initial Commit

---
+++ CraftDigitalCaseStudy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CraftDigitalCaseStudy
+//
+//  Created by CDGTaxi on 30/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
